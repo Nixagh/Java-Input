db769b1f10d747c4cb7754986a70e9bd5e1212cd
nghia.nguyen-dinh <nghia.nguyen-dinh@banvien.com.vn>
2023-11-09 11:46:33 +0700

    test

---
+++ src/main/java/com/nixagh/contentinput/entities/ResourceEntity.java
@@ -0,0 +1,37 @@
+package com.nixagh.contentinput.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author nghia.nguyen-dinh
+ * @since 11/9/2023 at 11:33 AM
+ */
+@Entity
+@Table(name = "resource")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResourceEntity {
+    @Id
+    @Column(name = "resourceid")
+    private Long resourceId;
+    @Column(name = "resourcecode")
+    private String resourceCode;
+    @Column(name = "productid")
+    private Long productId;
+    @Column(name = "programtocid")
+    private Long programTocId;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "adaptiveresourcetype")
+    private String adaptiveResourceType;
+}
---
+++ src/main/java/com/nixagh/contentinput/repository/ResourceRepository.java
@@ -0,0 +1,24 @@
+package com.nixagh.contentinput.repository;
+
+import com.nixagh.contentinput.entities.ResourceEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.persistence.Tuple;
+import java.util.List;
+
+/**
+ * @author nghia.nguyen-dinh
+ * @since 11/9/2023 at 11:36 AM
+ */
+public interface ResourceRepository extends JpaRepository<ResourceEntity, Long> {
+    @Query(value =
+        "select r.resourcecode, r.resourceid , r.description , p.productid , r.adaptiveresourcetype " +
+        "from resource r " +
+        "join product p on p.productid =r.productid " +
+        "join programtoc p2 on p2.programtocid = r.programtocid " +
+        "where p.code = :code " +
+        "and p2.\"name\" = :unit " +
+        "and r.adaptiveresourcetype = :adaptiveResourceType ", nativeQuery = true)
+    List<Tuple> getResourceCodes(String code, String unit, String adaptiveResourceType);
+}
