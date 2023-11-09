6b31b3b83daf1a89458222fd9dba8dda4a47685d
BANVIENCORP\thuan.pham-van <thuan.pham-van@banvien.com.vn>
2023-11-09 14:20:27 +0700

    WordListService

---
+++ src/main/java/com/nixagh/contentinput/service/WordListService.java
@@ -0,0 +1,18 @@
+package com.nixagh.contentinput.service;
+
+import com.nixagh.contentinput.repository.PassageRepository;
+import com.nixagh.contentinput.repository.QuestionRepository;
+import com.nixagh.contentinput.service.IP.SP.SPService;
+import com.nixagh.contentinput.util.ExcelReader;
+import javax.persistence.EntityManager;
+
+public class WordListService extends SPService {
+
+    public WordListService(ExcelReader excelReader,
+        QuestionRepository questionRepository,
+        PassageRepository passageRepository,
+        EntityManager entityManager) {
+        super(excelReader, questionRepository, passageRepository, entityManager);
+    }
+
+}
